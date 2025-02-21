3529ba7c56e8d7253663237a6a7e48e5bc6a4b4f
Ace <PrimeCoder.Authors@gmail.com>
2025-02-21 11:19:17 +1100

    Initial Commit

---
+++ BindingTest/BindingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingTestApp.swift
+//  BindingTest
+//
+//  Created by ace on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct BindingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
